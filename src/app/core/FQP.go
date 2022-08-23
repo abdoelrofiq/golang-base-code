cf8ca840bf73662e25305ae6b807428c53b3908b
abdoelrofiq <abdul.rofiq2308@gmail.com>
2022-08-23 07:10:23 +0700

    chore: update FQP

--- src/app/core/FQP.go
+++ src/app/core/FQP.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -22,11 +23,14 @@ func getBracketIndex(value string) (int, int) {
 	return firstBracketIndex, secondBracketIndex
 }
 
-func valueTypeChecker(value string) string {
+func valueTypeList(value string) string {
 
 	types := map[string]interface{}{
-		"INT":    "int",
-		"STRING": "string",
+		"INT":     "int",
+		"STRING":  "string",
+		"DATE":    "date",
+		"ARRAY":   "array",
+		"BOOLEAN": "boolean",
 	}
 
 	if types[value] != nil {
@@ -37,13 +41,90 @@ func valueTypeChecker(value string) string {
 
 }
 
-func FQPBuilder(c echo.Context) (string, interface{}, error) {
-	var replacementName string
-	var filterQueryString string
-	var valueType string
+func extractValueType(value string) string {
+	firstBracketIndex, secondBracketIndex := getBracketIndex(value)
+	return strings.ToUpper(string(value[firstBracketIndex+1 : secondBracketIndex]))
+}
 
+func valueTypeChecker(value string) (string, error) {
+	rawValueType := extractValueType(value)
+	valueType := valueTypeList(rawValueType)
+	if len([]rune(valueType)) == 0 {
+		return valueType, errors.New(fmt.Sprint("value type of ", rawValueType, " is not supported for now."))
+	}
+
+	return valueType, nil
+}
+
+func valueConverter(value string, valueType string) interface{} {
+	var newValue interface{}
+
+	switch valueType {
+	case valueTypeList("INT"):
+		integerValue, _ := strconv.Atoi(value)
+		newValue = integerValue
+	case valueTypeList("DATE"):
+		// value type of date will be string always
+		newValue = value
+	case valueTypeList("ARRAY"):
+		newValue = arrayValueBuilder(value)
+	case valueTypeList("BOOLEAN"):
+		booleanValue, _ := strconv.ParseBool(strings.TrimSpace(value))
+		newValue = booleanValue
+	default:
+		// default value type is string
+		newValue = value
+	}
+
+	return newValue
+}
+
+func arrayValueBuilder(value string) interface{} {
+	firstBracketIndex, secondBracketIndex := getBracketIndex(value)
+	if firstBracketIndex == 0 && secondBracketIndex > 0 {
+		valueType, _ := valueTypeChecker(value)
+		if valueType != valueTypeList("ARRAY") {
+			var arrayValue []interface{}
+			for _, element := range strings.Split(value[secondBracketIndex+2:len([]rune(value))-1], ",") {
+				arrayValue = append(arrayValue, valueConverter(element, valueType))
+			}
+
+			return arrayValue
+		}
+	}
+
+	return nil
+}
+
+func argumentValueBuilder(element []string, valueType string) (interface{}, error) {
+	value := element[1]
+
+	return valueConverter(value, valueType), nil
+}
+
+func replacementNameBuilder(element []string) (string, string, error) {
+	var valueType string
+	rawReplacementName := element[0]
+	firstBracketIndex, secondBracketIndex := getBracketIndex(rawReplacementName)
+
+	if firstBracketIndex > 0 && secondBracketIndex > 0 {
+		// value type checker
+		valueType, err := valueTypeChecker(rawReplacementName)
+		if err != nil {
+			return rawReplacementName, valueType, errors.New(err.Error())
+		}
+
+		// create replacement name
+		return rawReplacementName[1:firstBracketIndex], valueType, nil
+	}
+
+	return rawReplacementName, valueType, errors.New("failed to create replacement name")
+}
+
+func FQPBuilder(c echo.Context) (string, interface{}, error) {
 	filterQuery := c.QueryParam("filter-query")
 	rawfilterArgument := c.QueryParam("filter-argument")
+
 	if len([]rune(filterQuery)) == 0 && len([]rune(rawfilterArgument)) == 0 {
 		return "", nil, nil
 	}
@@ -58,45 +139,28 @@ func FQPBuilder(c echo.Context) (string, interface{}, error) {
 
 	filter := Filter{FilterQuery: filterQuery, FIlterArgument: rawfilterArgument}
 	filterArgument := map[string]interface{}{}
-	filterQueryString = filter.FilterQuery
+	filterQueryString := filter.FilterQuery
 
 	//convert filter argument from string to array
-	newFilterArgument := strings.Split(filter.FIlterArgument, "&&")
+	filterArgumentInArray := strings.Split(filter.FIlterArgument, "&&")
 
-	for _, element := range newFilterArgument {
+	for _, element := range filterArgumentInArray {
 
 		//convert element  from string to array
-		newElement := strings.Split(element, "=")
+		rawArgument := strings.Split(element, "=")
 
-		for _, rawValue := range newElement {
-			firstBracketIndex, secondBracketIndex := getBracketIndex(rawValue)
-
-			if firstBracketIndex != -1 && secondBracketIndex != -1 {
-				// value type checker
-				rawValueType := strings.ToUpper(string(rawValue[firstBracketIndex+1 : secondBracketIndex]))
-				valueType = valueTypeChecker(rawValueType)
-				if len([]rune(valueType)) == 0 {
-					return filterQueryString, filterArgument, errors.New("value type of " + rawValueType + " is not supported for now.")
-				}
-
-				// create replacement name
-				replacementName = rawValue[1:firstBracketIndex]
-			}
-
-			// create argument value
-			switch valueType {
-			case valueTypeChecker("INT"):
-				integerValue, _ := strconv.Atoi(rawValue)
-				filterArgument[replacementName] = integerValue
-			case valueTypeChecker("DATE"):
-				// value type of date will be string always
-				filterArgument[replacementName] = rawValue
-			default:
-				// default value type is string
-				filterArgument[replacementName] = rawValue
-			}
-
+		replacementName, valueType, err := replacementNameBuilder(rawArgument)
+		if err != nil {
+			return filterQueryString, filterArgument, errors.New(err.Error())
 		}
+
+		argumentValue, err := argumentValueBuilder(rawArgument, valueType)
+		if err != nil {
+			return filterQueryString, filterArgument, errors.New(err.Error())
+		}
+
+		// create argument value
+		filterArgument[replacementName] = argumentValue
 
 	}
 
@@ -116,6 +180,6 @@ func FQP(DB *gorm.DB, c echo.Context) (*gorm.DB, error) {
 	} else {
 		queryDB = DB.Where(filterQueryString, filterArgument)
 	}
-
+	fmt.Println(filterQueryString, filterArgument)
 	return queryDB, nil
 }
